005dc281ddc77b78255fbfa98eaf922755154afe
Alexander <144239138+SapteinKabeltann@users.noreply.github.com>
2024-08-06 16:48:10 +0200

    added func to open products

--- pages/product/[[id]].vue
+++ pages/product/[[id]].vue
@@ -5,8 +5,10 @@
       <NuxtLink to="/kjope" class="nav-link">Kjøp</NuxtLink>
       <NuxtLink to="/selge" class="nav-link">Salg</NuxtLink>
     </nav>
+
+
     <div class="product-content" v-if="product">
-      <img :src="product.image" alt="" class="product-image" />
+       <img :src="`https://msjupohbqsbqzyjqjdop.supabase.co/storage/v1/object/public/${product.image_url}`" :alt="product.name" class="product-image" />
       <h2>{{ product.description }}</h2>
       <p>Mer beskrivelse om produktet...</p>
     </div>
@@ -16,19 +18,24 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
-
+const supabase = useSupabaseClient();
 const route = useRoute();
 const product = ref(null);
 
-const fetchProduct = (id) => {
-  // Dummy data for demonstration
-  const productData = [
-    { id: 1, image: 'path/to/image1.jpg', description: 'Produkt 1' },
-    { id: 2, image: 'path/to/image2.jpg', description: 'Produkt 2' },
-    { id: 3, image: 'path/to/image3.jpg', description: 'Produkt 3' },
-    // ... flere produkter
-  ];
-  product.value = productData.find((p) => p.id === parseInt(id));
+const fetchProduct = async (id) => {
+
+  const { data, error } = await supabase
+    .from('products')
+    .select("*")
+    .eq('id', id)
+    .maybeSingle()
+
+  if(error) {
+    console.error("finner ikke produkt id" , error);
+    return
+  }
+  
+  product.value = data 
 };
 
 onMounted(() => {
